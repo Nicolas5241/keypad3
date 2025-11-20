7eedcfedbfc2ffb6c3906224376906ae6287879c
Nicolas Kist Steffen <nicolasdesenvolvedor@gmail.com>
2025-11-20 12:04:45 -0300

    update example

--- examples/arduino-uno/src/main.rs
+++ examples/arduino-uno/src/main.rs
@@ -2,7 +2,6 @@
 #![no_main]
 
 use arduino_hal::{ Delay, default_serial };
-use embedded_hal::digital::InputPin;
 use panic_halt as _;
 use keypad3::{Keypad, KeypadLayout, Pins};
 
@@ -15,34 +14,40 @@ fn main() -> ! {
 	let mut serial = default_serial!(dp, pins, 57600);
 
 	let keypad_row_pins = &[
-		&pins.d5.into_pull_up_input().downgrade(),
-		&pins.d4.into_pull_up_input().downgrade(),
-		&pins.d3.into_pull_up_input().downgrade(),
 		&pins.d2.into_pull_up_input().downgrade(),
+		&pins.d3.into_pull_up_input().downgrade(),
+		&pins.d4.into_pull_up_input().downgrade(),
+		&pins.d5.into_pull_up_input().downgrade(),
 	];
 
 	let keypad_column_pins = &mut [
 		&mut pins.d9.into_opendrain().downgrade(),
 		&mut pins.d8.into_opendrain().downgrade(),
 		&mut pins.d7.into_opendrain().downgrade(),
-		&mut pins.d6.into_opendrain().downgrade(),
 	];
 
 	let keypad_pins = Pins::new(keypad_row_pins, keypad_column_pins);
 
 	let keypad_layout: KeypadLayout<_> = &[
-		&['1', '2', '3', 'A'],
-		&['4', '5', '6', 'B'],
-		&['7', '8', '9', 'C'],
-		&['*', '0', '#', 'D'],
+		&['1', '2', '3'],
+		&['4', '5', '6'],
+		&['7', '8', '9'],
+		&['*', '0', '#'],
 	];
 
 	let mut keypad = Keypad::new(keypad_pins, keypad_layout);
+
+	let mut key_pressed = false;
 	
     loop {
 		let key = keypad.read_char(&mut delay);
 		if let Some(key) = key {
-			ufmt::uwriteln!(&mut serial, "{}", key);
+			if !key_pressed {
+				ufmt::uwriteln!(&mut serial, "{}", key);
+				key_pressed = true;
+			}
+		} else {
+		    key_pressed = false;
 		}
     }
 }
